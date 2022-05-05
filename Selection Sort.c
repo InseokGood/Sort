a1de0d6a9d2fee74e1643eb07d27f82a03e30d52
InseokGood <101339244+InseokGood@users.noreply.github.com>
2022-05-05 11:47:07 +0900

    Update Selection Sort.c

--- Selection Sort.c
+++ Selection Sort.c
@@ -1,3 +1,35 @@
 ```c
-#include
+//일반 선택정렬 방법 2 에 10승
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 1024
+#define SWAP(x,y,t) ((t)=(x),(x)=(y),(y)=(t))
+
+int list[MAX_SIZE];
+int n;
+void selection_sort(int list[],int n)//선택 정렬 함수
+{
+  int i,j,least,temp;
+  for(int i=0;i<n-1;i++){
+    least = i;
+    for(j=i+1;j<n;j++) // 최솟값 탐색
+      if(list[j]<list[least]) least=j;
+    SWAP(list[i],list[least],temp);//찾은 최솟값 옮기기
+  }
+}
+int main(void) {
+  int i;
+  n = MAX_SIZE;
+  srand(time(NULL));//랜덤으로 값 넣기
+for(i=0;i<n;i++){ 
+  list[i]=rand() % 200; //난수 발생 범위 0~199
+}  
+  selection_sort(list,n);
+  for(i=0;i<n;i++)
+    printf("%d ",list[i]);
+    
+  printf("\n");
+  
+  return 0;
+}
 ```
